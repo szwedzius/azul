804244f3b3c3c8876ccdfb9b20fafd63a7122d9e
Rafał <01178280@pw.edu.pl>
2023-05-09 10:42:52 +0200

    Added tests to PatterLines

---
+++ Tests/PatternLinesTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class PatternLinesTest {
+    @Test
+    void addToRowTest(){
+        Tile[][] test = new Tile[5][5];
+        for (int i = 0; i < 3; i++)
+            test[3][i] = Tile.BLACK;
+        PatternLines patternLines = new PatternLines();
+        patternLines.addToRow(3,Tile.BLACK,3);
+        assertArrayEquals(patternLines.rows, test);
+    }
+    @Test
+    void addToRowTest2(){
+        Tile[][] test = new Tile[5][5];
+        for (int i = 0; i < 5; i++)
+            test[4][i] = Tile.WHITE;
+        PatternLines patternLines = new PatternLines();
+        patternLines.addToRow(4,Tile.WHITE,5);
+        assertArrayEquals(patternLines.rows, test);
+    }
+    @Test
+    void isRowFullTest(){
+        PatternLines patternLines = new PatternLines();
+        assertFalse(patternLines.isRowFull(4));
+    }
+    @Test
+    void isRowFullTest2(){
+        PatternLines patternLines = new PatternLines();
+        patternLines.addToRow(0, Tile.WHITE,1);
+        assertTrue(patternLines.isRowFull(0));
+    }
+}

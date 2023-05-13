dd7aba0cf79455f24eec1207c254b2fbbe1939cb
Mateusz Orzełowski <01178298@pw.edu.pl>
2023-05-14 00:11:58 +0200

    Updated Wall.java

--- src/Wall.java
+++ src/Wall.java
@@ -3,26 +3,31 @@
  * on each player's board
  */
 public class Wall {
+    /**
+     * Matrix indicating which of the places on player's wall are filled
+     */
+    private final boolean[][] filled;
+
+    /**
+     * Matrix of tiles on the wall creating the pattern
+     */
+    private final Tile[][] pattern;
 
     /**
      * Default constructor
      */
     public Wall(){
+        filled = new boolean[][]{{false, false, false, false, false},
+                                 {false, false, false, false, false},
+                                 {false, false, false, false, false},
+                                 {false, false, false, false, false},
+                                 {false, false, false, false, false}};
+        pattern = new Tile[][]{{Tile.BLUE, Tile.YELLOW, Tile.RED, Tile.BLACK, Tile.WHITE},
+                               {Tile.WHITE, Tile.BLUE, Tile.YELLOW, Tile.RED, Tile.BLACK},
+                               {Tile.BLACK, Tile.WHITE, Tile.BLUE, Tile.YELLOW, Tile.RED},
+                               {Tile.RED, Tile.BLACK, Tile.WHITE, Tile.BLUE, Tile.YELLOW},
+                               {Tile.YELLOW, Tile.RED, Tile.BLACK, Tile.WHITE, Tile.BLUE}};
     }
-
-    /**
-     * Matrix indicating which of the places on player's wall are filled
-     */
-    private boolean[][] filled;
-
-    /**
-     * Matrix of tiles on the wall creating the pattern
-     */
-    private final Tile[][] pattern = {{Tile.BLUE, Tile.YELLOW, Tile.RED, Tile.BLACK, Tile.WHITE},
-                                     {Tile.WHITE, Tile.BLUE, Tile.YELLOW, Tile.RED, Tile.BLACK},
-                                     {Tile.BLACK, Tile.WHITE, Tile.BLUE, Tile.YELLOW, Tile.RED},
-                                     {Tile.RED, Tile.BLACK, Tile.WHITE, Tile.BLUE, Tile.YELLOW},
-                                     {Tile.YELLOW, Tile.RED, Tile.BLACK, Tile.WHITE, Tile.BLUE}};
 
     /**
      * Placing a tile on the wall
@@ -31,54 +36,70 @@ public class Wall {
      * @return Number of points scored for the tile's placement
      */
     public int addTile(Tile tile, int row) {
-        int whereTileIsAdded = 0;
-        int isAdjacent = 0;
-        // adding tile
+        int column = 0;
         for (int i = 0; i < 5; i++){
             if (pattern[row][i] == tile){
                 filled[row][i] = true;
-                whereTileIsAdded = i;
+                column = i;
                 break;
             }
         }
-        return countFilledTiles(whereTileIsAdded, row);
+        return countPointsForTile(column, row);
     }
 
-    private int countFilledTiles(int whereTileIsAdded, int row) {
+    private int countPointsForTile(int column, int row) {
         int horizontalPoints = 0;
         int verticalPoints = 0;
 
         // checking horizontally
-        for (int i = whereTileIsAdded - 1; i >= 0; i--){
+        for (int i = column - 1; i >= 0; i--){
             if (!filled[row][i])
                 break;
             horizontalPoints++;
         }
 
-        for (int i = whereTileIsAdded + 1; i < 5; i++){
+        for (int i = column + 1; i < 5; i++){
             if (!filled[row][i])
                 break;
             horizontalPoints++;
         }
 
         // checking vertically
-        for (int i = whereTileIsAdded - 1; i >= 0; i--){
-            if (!filled[i][whereTileIsAdded])
+        for (int i = column - 1; i >= 0; i--){
+            if (!filled[i][column])
                 break;
             verticalPoints++;
         }
 
-        for (int i = whereTileIsAdded + 1; i < 5; i++){
-            if (!filled[i][whereTileIsAdded])
+        for (int i = column + 1; i < 5; i++){
+            if (!filled[i][column])
                 break;
             verticalPoints++;
         }
 
-        // +1 because we skipped one tile
-        if (horizontalPoints + verticalPoints > 0)
-            return verticalPoints + horizontalPoints + 1;
-        else
-            return 1;
+        if(verticalPoints == 4)
+            verticalPoints += 7;
+        if(horizontalPoints == 4)
+            verticalPoints += 2;
+        if(horizontalPoints > 0)
+            horizontalPoints++;
+        if(verticalPoints > 0)
+            verticalPoints++;
+        if(verticalPoints + horizontalPoints == 0)
+            verticalPoints++;
+
+        // BLACK, RED, WHITE, YELLOW, BLUE,
+        int[] amounts = new int[]{0, 0, 0, 0, 0};
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<5; j++) {
+                amounts[pattern[i][j].ordinal()] += 1;
+            }
+        }
+        for(int i=0; i<5; i++)
+            if(amounts[i] == 5)
+                horizontalPoints += 10;
+
+        return verticalPoints + horizontalPoints;
     }
 
     /**
@@ -89,7 +110,7 @@ public class Wall {
      * @return Result of the evaluation
      */
     public boolean colourCheck(Tile tile, int row) {
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             if (pattern[row][i] == tile && filled[row][i])
                 return true;
         }
@@ -107,6 +128,6 @@ public class Wall {
             if (!filled[row][i])
                 return false;
         }
-        return false;
+        return true;
     }
 }

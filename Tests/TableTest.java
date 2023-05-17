b8da3d20a21e8b19ec95e184c91494ca1791c72c
Maja <01178264@pw.edu.pl>
2023-05-17 23:27:19 +0200

    added TableTest

---
+++ Tests/TableTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableTest {
+
+    Table table = new Table(2);
+
+    @Test
+    void isPriorityTileInCenterTest() {
+        assertEquals(table.isPriorityTileInCenter(), true);
+    }
+
+    @Test
+    void isBagEmpty() {
+        assertEquals(false, table.isBagEmpty());
+    }
+
+    @Test
+    void refillBagTest() throws Exception {
+        Exception exception = Assertions.assertThrows(Exception.class, () -> table.refillBag());
+        String expectedMessage = "Can't refill bag until it is not empty";
+        String actualMessage = exception.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage,
+                "The exception message should match the expected message");
+    }
+}

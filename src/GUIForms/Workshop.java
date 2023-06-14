4a7744143343bbb318c36ecc5576d680e6bada07
Karol <karol.zelazowski.stud@pw.edu.pl>
2023-06-14 23:22:20 +0200

    Workshop layout. Confirm button as placeHolder of Board button

--- src/GUIForms/Workshop.java
+++ src/GUIForms/Workshop.java
@@ -1,6 +1,7 @@
 package GUIForms;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Workshop {
     private JPanel workshop;
@@ -14,13 +15,66 @@ public class Workshop {
     private JLabel score3;
     private JLabel score2;
     private JLabel score1;
+    private JButton boardButton;
+    private JButton centerButton;
+    private JPanel factory1;
+    private JPanel center;
+    private JPanel factory2;
+    private JPanel factory3;
+    private JPanel factory4;
+    private JPanel factory5;
+    private JPanel factory6;
+    private JPanel factory7;
+    private JPanel factory8;
+    private JPanel factory9;
+    private JLabel scoreboard;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
+    private JButton button21;
+    private JButton button22;
+    private JButton button23;
+    private JButton button24;
+    private JButton button25;
+    private JButton button26;
+    private JButton button27;
+    private JButton button28;
+    private JButton button29;
+    private JButton button30;
+    private JButton button31;
+    private JButton button32;
+    private JButton button33;
+    private JButton button34;
+    private JButton button35;
+    private JButton button36;
     private JTextField textField1;
 
     public JPanel getWorkshopPanel() {
         return workshop;
     }
 
+    Workshop() throws HeadlessException  {
+        scoreboard.setIcon(HelpfulMethodsGuiJava.getImageIconWithSize("img/scoreboard.png",339,90));
 
+        HelpfulMethodsGuiJava.createButton("img/confirm.png",291,150,boardButton);
+    }
 
 
 
